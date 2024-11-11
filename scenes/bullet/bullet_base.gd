db21fd5fc74628ed2e59976036cb9bb7428a196c
narutocyx <narutocyxlive@163.com>
2024-11-11 19:52:57 +0800

    Game 4 - 137.11

---
+++ scenes/bullet/bullet_base.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+class_name Bullet
+
+var _direction: Vector2 = Vector2.RIGHT
+var _life_span: float = 5.0
+var _life_time: float = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += _direction * delta * 200.0
+	check_expired(delta)
+	
+func check_expired(delta: float) -> void:
+	_life_time += delta
+	if _life_time > _life_span:
+		queue_free()
+
+func setup(pos: Vector2, dir: Vector2, speed: float, ls: float) -> void:
+	_direction = dir.normalized() * speed
+	_life_span = ls
+	global_position = pos
+
+func _on_area_entered(area: Area2D) -> void:
+	queue_free()
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,31 +2,81 @@ extends CharacterBody2D
 
 class_name Player
 
-const GRAVITY: float = 100.0
+enum PlayerState { IDLE, RUN, JUMP, FALL, HURT }
+
+const GRAVITY: float = 690.0
+const RUN_SPEED: float =120.0
+const MAX_FALL: float =400
+const JUMP_VELOCITY: float = -260.0
+
+var _state: PlayerState = PlayerState.IDLE
 
 @export var SPEED: float = 120.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var debug_label: Label = $DebugLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if is_on_floor() == false:
 		velocity.y += GRAVITY * delta
-	Get_Input()
+	
+	get_input()
 	move_and_slide()
+	calculate_states()
+	update_debug_label()
 
-func Get_Input() -> void:
-	velocity.x = 0
-	#animated_sprite_2d.play("idle")	
+func get_input() -> void:
+		
 	if Input.is_action_pressed("move_left") == true:
-		velocity.x = -SPEED
+		velocity.x = -RUN_SPEED
 		animated_sprite_2d.flip_h = true
-		animated_sprite_2d.play("run")
 	elif Input.is_action_pressed("move_right") == true:
-		velocity.x = SPEED
+		velocity.x = RUN_SPEED
 		animated_sprite_2d.flip_h = false
-		animated_sprite_2d.play("run")
+	else:
+		velocity.x = 0
+		
+	if Input.is_action_just_pressed("jump") == true and is_on_floor() == true:
+		velocity.y = JUMP_VELOCITY
+	
+	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
+
+#states
+func calculate_states() -> void:
+	
+	if is_on_floor() == true:
+		if velocity.x == 0:
+			set_state(PlayerState.IDLE)
+		else:
+			set_state(PlayerState.RUN)
+	else:
+		if velocity.y < 0:
+			set_state(PlayerState.JUMP)
+		else:
+			set_state(PlayerState.FALL)
+
+func set_state(new_state: PlayerState) -> void:
+	if new_state == _state:
+		return
+	_state = new_state
+	
+	match _state:
+		PlayerState.IDLE:
+			animated_sprite_2d.play("idle")
+		PlayerState.JUMP:
+			animated_sprite_2d.play("jump")
+		PlayerState.RUN:
+			animated_sprite_2d.play("run")
+		PlayerState.FALL:
+			animated_sprite_2d.play("fall")
+		
+#debug
+func update_debug_label() -> void:
+	debug_label.text = "floor:%s\n%s\n%.0f,%.0f" % [
+		is_on_floor(),
+		PlayerState.keys()[_state],
+		velocity.x, velocity.y]
